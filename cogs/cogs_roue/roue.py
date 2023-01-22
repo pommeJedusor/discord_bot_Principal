cc07c129dcd4bf65ca4dfea1fa707b58d0f7b1ef
Pomme Jedusor <lemonniergregory2@gmail.com>
2023-01-22 13:03:58 +0100

    les bots n'apparraitrons plus dans les tirages

--- cogs/cogs_roue/roue.py
+++ cogs/cogs_roue/roue.py
@@ -10,9 +10,13 @@ class RoueDeLaGrosseMerde(commands.Cog):
 
     @app_commands.command(name="roue_de_la_grosse_merde",description="tourner la roue de la grosse merde")
     async def roue_de_la_grosse_merde(self,interaction:discord.Interaction):
-        alea_number = random.randint(0,interaction.guild.member_count-1)
+        member_filtered = []
+        for member in interaction.guild.members:
+            if not member.bot:
+                member_filtered.append(member)
+        alea_number = random.randint(0,len(member_filtered)-1)
         await interaction.response.send_message(f"suspens...",ephemeral=True)
-        await interaction.channel.send(f"<@{interaction.guild.members[alea_number].id}>")
+        await interaction.channel.send(f"<@{member_filtered[alea_number].id}>")
         await interaction.channel.send("https://cdn.discordapp.com/attachments/743439125914320986/1066684359165345932/Snapchat-59690717.jpg")
 
 
