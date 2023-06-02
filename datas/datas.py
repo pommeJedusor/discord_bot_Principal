976a8fb62d558594843bee880b6cd5e74d110dc1
PommeJedusor <gregou.lemonnier@gmail.com>
2023-06-02 16:27:56 +0200

    retrait du fichier txt qui stockait les données

--- datas/datas.py
+++
@@ -1,8 +0,0 @@
-epicgame_channel = "your_channel_for_new_games"
-hearstone_channel = "your_channel_for_update"
-role_epicgame = "the role that you want to mension"
-role_hearstone = "the role that you want to mension"
-GUIL_ID = "Your guild ID"
-epicgame_file = "cogs/epicgames_hearstone/jeux_gratuits.txt"
-hearstone_file = "cogs/epicgames_hearstone/maj_hearstone.txt"
-BOT_TOKEN = "your discord token"
