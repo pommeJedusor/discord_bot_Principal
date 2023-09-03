a99c4710cf257f9fb5ba7f7186e3bb4fb312c07b
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-09-03 21:29:23 +0200

    add the \n that was forgotten

--- cogs/cogs_roue/roue.py
+++ cogs/cogs_roue/roue.py
@@ -19,7 +19,8 @@ class RoueDeLaGrosseMerde(commands.Cog):
         await interaction.channel.send(f"<@{member_filtered[alea_number].id}>")
         await interaction.channel.send("https://cdn.discordapp.com/attachments/743439125914320986/1066684359165345932/Snapchat-59690717.jpg")
         
-    @app_commands.command(name="pile_face",description="faites un pile ou face")    async def pile_face(self, interation:discord.Interaction):
+    @app_commands.command(name="pile_face",description="faites un pile ou face")
+    async def pile_face(self, interation:discord.Interaction):
         if random.randint(0,1):
             await interaction.response.send_message("pile")
         else:
